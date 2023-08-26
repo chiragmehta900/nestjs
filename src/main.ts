2f9248e4d3363fc465cdde1a00db816dba905cee
chiragmehta900 <chiragmehta900@gmail.com>
2023-08-26 12:24:22 +0530

    test

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
+
   await app.listen(3000);
+
 }
+
 bootstrap();
